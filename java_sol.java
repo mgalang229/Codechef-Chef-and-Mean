a2f62a94eec05ae5753a53ad05b25f4bbd0ac90c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-08 10:00:19 +0800

    Create java_sol.java

---
+++ java_sol.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+		int t=in.nextInt();
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
+	}
+	
+	static class Solver {
+		Solver() {
+			int n=in.nextInt();
+			double a[] = new double[n];
+			double sum=0;
+			for(int i=0; i<n; ++i) {
+				a[i]=in.nextDouble();
+				sum+=a[i];
+			}
+			for(int i=0; i<n; ++i)
+				if(sum/n==a[i]) {
+					out.println(i+1);
+					return;
+				}
+			out.println("Impossible");
+		}
+	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+
+}
